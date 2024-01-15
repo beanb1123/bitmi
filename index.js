21d3f68e8d70d633be53125ce5afbc7f9b99692b
beanb1123 <103089678+beanb1123@users.noreply.github.com>
2024-01-15 02:23:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -81,9 +81,7 @@ async function main() {
 
 		const hash = sha256(block_header);
 
-		
-
-		console.log("hash: ", hash)
+	//	console.log("hash: ", hash)
 	//	console.log("target: ", target)
 
 		if(lessThan(hash, target)) {
